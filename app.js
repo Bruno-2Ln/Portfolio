8634e32d0f6e04741d910de7a3b72fc985d82456
Bruno Delaine <bruno-d-34@hotmail.fr>
2021-02-01 13:56:24 +0100

    Utilisation de la bibliothèque type-writter

--- app.js
+++ app.js
@@ -17,3 +17,32 @@ if(window.matchMedia('(max-width: 1300px)')){
         })
     })
 }
+
+// Animation écriture
+
+const txtAnim = document.querySelector(".txt-animation");
+
+let typewriter = new Typewriter(txtAnim, {
+    loop: false,
+    deleteSpeed: 20
+})
+
+typewriter
+.pauseFor(1800)
+.changeDelay(30)
+.typeString('Bonjour, je suis Bruno Delaine')
+.pauseFor(300)
+.typeString('<strong>, Développeur Full-Stack</strong> !')
+.pauseFor(1000)
+.deleteChars(12)
+.typeString('<span style="color: #27ae60;"> CSS</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color: #EA39ff;"> PHP</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color: #ff6910;"> JavaScript</span> !')
+.pauseFor(1000)
+.deleteChars(12)
+.typeString('<span style="color: midnightblue;"> Angular</span> !')
+.start()
